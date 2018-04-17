4036465536c7b43c60acc14e492e6b1bddf51e03
Luiggi Mendoza <luiggi0925@gmail.com>
2018-04-16 19:01:31 -0500

    Struts 2 skelet.5on app

---
+++ src/main/java/pe/edu/cibertec/action/Prueba.java
@@ -0,0 +1,8 @@
+package pe.edu.cibertec.action;
+
+public class Prueba {
+
+    public static void main(String[] args) {
+        org.apache.struts2.dispatcher.filter.StrutsPrepareAndExecuteFilter x;
+    }
+}
